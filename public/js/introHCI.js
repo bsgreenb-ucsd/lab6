2be4847a142399bdf83062480f831bb22020bbba
Brian Greenberg <bsgreenb@eng.ucsd.edu>
2016-02-11 21:23:48 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,6 +25,8 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
+         $.get("/project/random",callback);
+         console.log("/project/"+idNumber);
 
 	console.log("User clicked on project " + idNumber);
 }
@@ -35,4 +37,22 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+        $.get("/palette",callbackColors);
+        
+}
+
+function callback(result) {
+  console.log(result);
+  var projectHTML = '<img src= "' + result['image'] + '"class="detailsImage">' + '<p><small>' + result['title'] + '</small></p>' + '<p><small>' + result['date'] + '</small></p>' + '<p>' + result['summary'] + '</p>';
+  console.log($("#project" + result.id + ".details").length);
+  $("#project" + result['id'] + " .details").html(projectHTML);
+}
+
+function callbackColors(result1) {
+  var colors = result1.colors.hex;
+  $('body').css('background-color', colors[0]);
+  $('.thumbnail').css('background-color', colors[1]);
+  $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+  $('p').css('color', colors[3]);
+  $('.project img').css('opacity', .75);
 }
